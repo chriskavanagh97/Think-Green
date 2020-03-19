6b36aa82a3b7c9d35fb55e763b6c2c43e04e422e
I352778 <c16492454@mydit.ie>
2020-03-19 16:28:36 +0000

    Added in transport analysis

---
+++ app/src/main/java/com/example/finalyearapp/Carbonfootprint/transport.java
@@ -0,0 +1,18 @@
+package com.example.finalyearapp.Carbonfootprint;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finalyearapp.R;
+
+public class transport extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transport);
+
+
+    }
+}
