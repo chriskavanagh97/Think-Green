12601bb66e8213fccfbd0442c164393b18cd223c
I352778 <c16492454@mydit.ie>
2020-03-23 16:33:55 +0000

    UI added

---
+++ app/src/main/java/com/example/finalyearapp/Maps/locations.java
@@ -0,0 +1,18 @@
+package com.example.finalyearapp.Maps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.finalyearapp.R;
+
+public class locations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_locations);
+
+
+    }
+}
