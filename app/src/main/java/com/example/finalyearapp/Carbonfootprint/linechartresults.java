97c5870d91fee7995853cc61b53a658e4b6323c1
I352778 <c16492454@mydit.ie>
2020-04-08 17:18:11 +0100

    step two of line chart complete

--- app/src/main/java/com/example/finalyearapp/Carbonfootprint/linechartresults.java
+++ app/src/main/java/com/example/finalyearapp/Carbonfootprint/linechartresults.java
@@ -23,6 +23,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 
 import org.w3c.dom.Text;
@@ -31,16 +34,9 @@ import java.util.ArrayList;
 
 public class linechartresults extends AppCompatActivity {
 
-    LineChart linechart;
-    Button display;
-    TextView results;
-    float score;
-    int total = 0;
-    private FirebaseAuth mFirebaseAuth;
-    private FirebaseAuth.AuthStateListener mAuthStateListener;
-    DatabaseReference mRootRef = FirebaseDatabase.getInstance().getReference();
-    ArrayList<Float> dataVals2 = new ArrayList<>();
-    ArrayList<Entry> dataVals = new ArrayList<>();
+
+    GraphView graph;
+
 
     ArrayList<ILineDataSet> lineDataSets = new ArrayList<>();
 
@@ -48,107 +44,40 @@ public class linechartresults extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linechart);
-        FirebaseAuth mFirebaseAuth = FirebaseAuth.getInstance();
-        String userid = mFirebaseAuth.getCurrentUser().getUid();
 
-        for (total = 0; total < 4; total++) {
-            DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference().child("Carbonfootprint").child(userid).child(String.valueOf(total));
-            databaseReference.addValueEventListener(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    final LineChartresult linechats = dataSnapshot.getValue(LineChartresult.class);
-                    score = linechats.getCarbonfootprint();
 
-                    dataVals2.add(score);
-                    //float confirm = total.floatValue();
-                    //Toast.makeText(linechartresults.this, "Display screen activated" + score, Toast.LENGTH_SHORT).show();
+        graph = findViewById(R.id.linechart);
 
-                }
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(getDataPoint());
 
-                }
-            });
-        }
-       /* Toast.makeText(linechartresults.this, "Float array:" + dataVals2  , Toast.LENGTH_SHORT).show();
-            linechart = (LineChart) findViewById(R.id.linechart);
-            Display();
-           /* Intent i = getIntent();
-            float[] dataVals2 = i.getFloatArrayExtra("results");
-            Toast.makeText(linechartresults.this, "New Array" + dataVals2, Toast.LENGTH_SHORT).show();
-            Toast.makeText(linechartresults.this, "Display screen activated" + dataVals2, Toast.LENGTH_SHORT).show();*/
-
-        display = findViewById(R.id.display);
-        display.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                display.setVisibility(display.GONE);
-              //  Toast.makeText(linechartresults.this, "Float array: on start" + dataVals2, Toast.LENGTH_SHORT).show();
-              //  Toast.makeText(linechartresults.this, "Float array: on start" + total, Toast.LENGTH_SHORT).show();
-                linechart = (LineChart) findViewById(R.id.linechart);
-                Display();
-
-            }
-        });
     }
 
+    private DataPoint[] getDataPoint() {
 
-    public void Display(){
-
-        results = findViewById(R.id.results);
-
-       // Toast.makeText(linechartresults.this, "Float array:" + dataVals2  , Toast.LENGTH_SHORT).show();
-
-        dataVals.add(new Entry(0,dataVals2.get(0)));
-        dataVals.add(new Entry(1,dataVals2.get(1)));
-        dataVals.add(new Entry(2,dataVals2.get(2)));
-        dataVals.add(new Entry(3,dataVals2.get(3)));
-        /*dataVals.add(new Entry(0,12));
-        dataVals.add(new Entry(1,10));
-        dataVals.add(new Entry(2,9));
-        dataVals.add(new Entry(3,11));*/
+        DataPoint[] dp = new DataPoint[]
+                {
+                        new DataPoint(0,1),
+                        new DataPoint(2,5),
+                        new DataPoint(3,1),
+                        new DataPoint(5,6),
+                        new DataPoint(8,3),
 
 
-
-
-
-        LineDataSet lineDataSet1 = new LineDataSet(dataVals,"data set 1");
-        lineDataSet1.setDrawCircles(true);
-        lineDataSet1.setCircleColor(Color.RED);
-        lineDataSet1.setCircleHoleRadius(6);
-        lineDataSet1.setColor(Color.BLUE);
-
-        lineDataSets.add(lineDataSet1);
-
-
-        linechart.setData(new LineData(lineDataSets));
-        linechart.setVisibleXRangeMaximum(65f);
-        linechart.invalidate();
-
-        float difference = dataVals2.get(3)- dataVals2.get(0);
-        float differencepositive = dataVals2.get(0)- dataVals2.get(3);
-        float total = dataVals2.get(0) + dataVals2.get(1) + dataVals2.get(2) + dataVals2.get(3);
-        float average = total / 4;
-        float nationalaverage =14;
-        float posave = nationalaverage - average;
-
-
-        if(difference< 0 & average < nationalaverage)
-        {
+       /* if (difference < 0 & average < nationalaverage) {
             results.setText("Congratulations your footprint is improving you have lowered you footprint by: " + differencepositive + "\n" +
-            "Your average result is: " + average + " Keep up the good work" + "\n" +
-                    "Your average is  " + posave + " lower than the national average " +  " Way to lead by example ");
+                    "Your average result is: " + average + " Keep up the good work" + "\n" +
+                    "Your average is  " + posave + " lower than the national average " + " Way to lead by example ");
 
-        }
-        else
-        {
-            results.setText("Your footprint has increased by " + difference + " from your first test this is not good "+ "\n" +
-                    " Your average result is: " + average +  " Please revisit our tips section to lower your footprint" + "\n" +
+        } else {
+            results.setText("Your footprint has increased by " + difference + " from your first test this is not good " + "\n" +
+                    " Your average result is: " + average + " Please revisit our tips section to lower your footprint" + "\n" +
                     "your average is  " + (average - nationalaverage) + " ");
 
-        }
+        }*/
 
-    }
+
+    };
+        return (dp);
+}
 }
